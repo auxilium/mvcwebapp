13598fa701ce2f90baeb480e02a89f30b90ee4ab
Arjan van der Laan <avdlaan85@hotmail.com>
2016-03-18 11:49:20 +0100

    oa application db context een eigen classe gegeven

---
+++ MVCWebApp/Models/ApplicationDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCWebApp.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Video> Videos { get; set; }
+        public DbSet<Image> Images { get; set; }
+
+        public override int SaveChanges()
+        {
+            // From http://benjii.me/2014/03/track-created-and-modified-fields-automatically-with-entity-framework-code-first/
+            var entities = ChangeTracker.Entries().Where(x => x.Entity is BaseEntity && (x.State == EntityState.Added || x.State == EntityState.Modified));
+
+            foreach (var entity in entities)
+            {
+                if (entity.State == EntityState.Added)
+                    ((BaseEntity)entity.Entity).Created = DateTime.Now;
+                ((BaseEntity)entity.Entity).Modified = DateTime.Now;
+            }
+
+            return base.SaveChanges();
+        }
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
--- MVCWebApp/Models/Statics/Statics.cs
+++ MVCWebApp/Models/Statics/Statics.cs
@@ -9,6 +9,10 @@ namespace MVCWebApp.Models
     {
         public static class RoleNames
         {
+            /*
+                Define roles by what they can do, not by a name. See examples below
+            */
+
             public const string Administrator = "administrator"; // Aux only
             public const string CanManageUsers = "canmanageusers";
             public const string CanEdit = "canedit";
